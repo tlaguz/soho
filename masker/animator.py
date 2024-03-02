ba2f5c02098888f8a94ceaadfa953a7d794a8260
tlaguz <git@tlaguz.pl>
2024-03-02 14:03:07 +0100

    Add statistics plot to animator

--- masker/animator.py
+++ masker/animator.py
@@ -6,6 +6,7 @@ import PyQt5
 
 import matplotlib
 import torch
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from masker.fits_loader import FitsLoader
 from masker.labelers.create_labeler import create_labeler
@@ -30,6 +31,45 @@ class Animator:
         self.fits_repository = FitsRepository(dbconn)
         self.labeler = create_labeler()
         self.normalizer = create_normalizer()
+
+    def _show_ex_pdf(self, mask, axs, axs_statistics_x, axs_statistics_y):
+        # Normalize probabilities
+        probabilities = mask / mask.sum()
+
+        # Marginal distributions
+        x_distribution = np.sum(probabilities, axis=0)
+        y_distribution = np.sum(probabilities, axis=1)
+
+        flat_index = np.argmax(probabilities)
+        max_y, max_x = np.unravel_index(flat_index, probabilities.shape)
+
+        # Expected values
+        expected_x = np.sum(np.arange(probabilities.shape[1]) * x_distribution)
+        expected_y = np.sum(np.arange(probabilities.shape[0]) * y_distribution)
+
+        # Main 2D array
+        img = axs.imshow(mask, origin='lower', cmap='viridis', animated=True, norm=matplotlib.colors.PowerNorm(vmin=0, vmax=1, gamma=2))
+        img_exh = axs.axhline(expected_y, color='r', animated=True)
+        img_exv = axs.axvline(expected_x, color='r', animated=True)
+        img_maxh = axs.axhline(max_y, color='y', animated=True)
+        img_maxv = axs.axvline(max_x, color='y', animated=True)
+
+        # X marginal distribution
+        img_x_bar = axs_statistics_x.bar(range(len(x_distribution)), x_distribution, color='g', animated=True)
+        img_x = axs_statistics_x.scatter(expected_x, x_distribution[int(expected_x)], color='r', zorder=5, animated=True)
+
+        # Y marginal distribution
+        img_y_bar = axs_statistics_y.barh(range(len(y_distribution)), y_distribution, color='b', animated=True)
+        img_y = axs_statistics_y.scatter(y_distribution[int(expected_y)], expected_y, color='r', zorder=5, animated=True)
+
+        # Hide labels for marginal distribution plots
+        plt.setp(axs_statistics_x.get_xticklabels(), visible=False)
+        plt.setp(axs_statistics_y.get_yticklabels(), visible=False)
+
+        result = [img, img_exh, img_exv, img_x, img_y, img_maxh, img_maxv]
+        result.extend(img_x_bar)
+        result.extend(img_y_bar)
+        return img, result
 
     def animate(self, yhts, model_path):
         colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'white']
@@ -81,22 +121,21 @@ class Animator:
         axs_running_diff_manual_label = axs[1, 0]
 
         axs_training_label = axs[0, 1]
+        axs_statistics = axs[1, 1]
+        # Define axes for marginal distributions around axs[1, 1]
+        divider = make_axes_locatable(axs_statistics)
+        axs_statistics_x = divider.append_axes("top", 0.6, pad=0.1, sharex=axs_statistics)
+        axs_statistics_y = divider.append_axes("left", 0.6, pad=0.1, sharey=axs_statistics)
+        # Rotate the y distribution axes for vertical
+        axs_statistics_y.invert_xaxis()
 
         axs_ml_prediction = axs[0, 2]
         axs_ml_prediction_residuals = axs[1, 2]
 
         axs_raw_manual_label.set_title('Raw with manual label')
-        axs_raw_manual_label.norm = matplotlib.colors.LogNorm()
-
         axs_running_diff_manual_label.set_title('Running diff with manual label')
-        #axs_running_diff_manual_label.norm = matplotlib.colors.LogNorm()
-
         axs_ml_prediction.set_title('ML prediction on running diff')
-        axs_ml_prediction.norm = matplotlib.colors.LogNorm()
-
         axs_ml_prediction_residuals.set_title('ML prediction on running diff - residuals')
-        axs_ml_prediction_residuals.norm = matplotlib.colors.LogNorm()
-
         axs_training_label.set_title('Training label')
 
         ims = []
@@ -128,13 +167,15 @@ class Animator:
             else:
                 print(f"No previous fits for {filename}")
 
-            im1 = axs_raw_manual_label.imshow(image, animated=True, cmap="viridis", vmin=0, vmax=20, origin='lower')
+            im1 = axs_raw_manual_label.imshow(image, animated=True, cmap="viridis", vmin=-20, vmax=20, origin='lower')
             imdiff = axs_running_diff_manual_label.imshow(diff, animated=True, cmap="inferno", vmin=-0.5, vmax=0.5, origin='lower')
 
             im2 = None
             im2_text = None
             im2_residuals = None
             im_label = None
+            img_statistics = None
+            img_statistics_list = []
             if model_path is not None:
                 rows = [p[0] for p in points]
                 cols = [p[1] for p in points]
@@ -153,6 +194,8 @@ class Animator:
                 im2_residuals = axs_ml_prediction_residuals.imshow(residuals, vmin=-1, vmax=1, animated=True, origin='lower')
 
                 im_label = axs_training_label.imshow(label.squeeze(0), vmin=0, vmax=1, animated=True, origin='lower')
+
+                img_statistics, img_statistics_list = self._show_ex_pdf(mask, axs_statistics, axs_statistics_x, axs_statistics_y)
             else:
                 pass
 
@@ -163,6 +206,7 @@ class Animator:
                 artists.append(im2_text)
                 artists.append(im2_residuals)
                 artists.append(im_label)
+                artists.extend(img_statistics_list)
 
             # add points to im plot
             for p in points:
@@ -183,6 +227,7 @@ class Animator:
         fig.colorbar(imdiff, ax=axs_running_diff_manual_label, orientation='vertical')
         if im2 is not None:
             fig.colorbar(im2, ax=axs_ml_prediction, orientation='vertical')
+            fig.colorbar(img_statistics, ax=axs_statistics, orientation='vertical')
         if im2_residuals is not None:
             fig.colorbar(im2_residuals, ax=axs_ml_prediction_residuals, orientation='vertical')
         if im_label is not None:
